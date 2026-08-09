4b14cb0cf3d3888e532e723023798d73927b3a8e
jeya1811 <jeyavignesh1811@gmail.com>
2026-08-09 17:21:53 +0530

    [commit] incrementer && decrementer

---
+++ comb_ckts/decrementer/decrementer.v
@@ -0,0 +1,35 @@
+// Design Module
+
+module decrementer #(parameter Width= 1)(
+  input [Width-1:0] ip,
+  output [Width-1:0] op
+);
+assign op= ip- 'b1;
+endmodule
+
+// Testbench Module
+
+module tb_decrementer;
+localparam Width= 4;
+reg [Width-1:0] ip;
+wire [Width-1:0] op;
+integer i;
+
+decrementer #(.Width(Width)) dut(.ip(ip), .op(op));
+
+initial begin
+  for(i= 0; i< 2** Width; i+= 1) begin
+    ip= i; #10;
+  end
+  $finish;
+end
+
+initial begin
+  $dumpfile(".vcd");
+  $dumpvars(0, tb_decrementer);
+  $display("Bit Width= %0d", Width);
+  $display("|TIME|IP|OP|");
+  $display("|-|-|-|");
+  $monitor("|%0t|%b|%b|", $time, ip, op);
+end
+endmodule
